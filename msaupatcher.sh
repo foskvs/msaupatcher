6015c34e84cc0160a5afb1b760cb44dbd4be891b
foskvs <32709273+foskvs@users.noreply.github.com>
2021-07-18 10:29:52 +0200

    Improved script

--- msaupatcher.sh
+++ msaupatcher.sh
@@ -1,28 +1,67 @@
 #!/bin/zsh
 
 RESULT=0
+STATUS="false"
+NO_ARG=1
 
-if [[ ${1:0:2} == "--" ]];
+check_status () {
+    STATUS=$(launchctl print-disabled gui/$(id -u) | grep microsoft | cut -d ">" -f2- | cut -c2-)
+}
+
+enable_updater () {
+    launchctl enable gui/$(id -u)/com.microsoft.update.agent
+    check_status
+        if [[ ${STATUS} == "false" ]];
+        then
+        RESULT=1
+        fi
+}
+
+disable_updater () {
+    launchctl disable gui/$(id -u)/com.microsoft.update.agent
+    check_status
+        if [[ ${STATUS} == "true" ]];
+        then
+        RESULT=1
+        fi
+}
+
+no_args () {
+    if [[ $1 == "" ]];
+    then
+    NO_ARG=1
+    else
+    NO_ARG=0
+    fi
+}
+
+no_args
+
+if [ ${NO_ARG} -eq 1 ];
+then
+check_status
+    if [[ ${STATUS} == "false" ]];
+    then
+    echo "Disabling the updater..."
+    disable_updater
+    elif [[ ${STATUS} == "true" ]];
+    then
+    echo "Enabling the updater..."
+    enable_updater
+    fi
+elif [[ ${1:0:2} == "--" ]];
 then
 COMMAND_TO_RUN=$(echo $1 | cut -c3-)
     if [[ $COMMAND_TO_RUN == "enable" ]];
     then
-    launchctl enable gui/$(id -u)/com.microsoft.update.agent
-    FFF=$(launchctl print-disabled gui/$(id -u) | grep microsoft | cut -d ">" -f2- | cut -c2-)
-        if [[ ${FFF} == "false" ]];
-        then
-        RESULT=1
-        fi
+    enable_updater
     elif [[ $COMMAND_TO_RUN == "disable" ]];
     then
-    launchctl disable gui/$(id -u)/com.microsoft.update.agent
-    FFF=$(launchctl print-disabled gui/$(id -u) | grep microsoft | cut -d ">" -f2- | cut -c2-)
-        if [[ ${FFF} == "true" ]];
-        then
-        RESULT=1
-        fi
+    disable_updater
     fi
 fi
+
+
 
 if [ ${RESULT} -eq 1 ];
 then
